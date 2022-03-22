3dcede17dc263510273a7d3fc51b64e3d34bcee9
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-03-22 19:25:29 -0300

    Criando Nav, Home e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import Home from "./paginas/home/Home";
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
+import Home from './paginas/home/Home';
 import './App.css';
+
 
 function App() {
   return (
-      <Home />
+    <>
+      <Navbar />
+       <Home />
+      <Footer />
+    </>
   );
 }
 
 export default App;
-
-/*
-<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */
