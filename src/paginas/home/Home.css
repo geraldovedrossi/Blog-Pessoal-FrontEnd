96225d4065827530216771b249967f56a138a473
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-03-22 00:25:20 -0300

    Reformulando pastas

---
+++ src/paginas/home/Home.css
@@ -0,0 +1,17 @@
+.titulo {
+    color: green;
+    text-align: center;
+}
+
+.subtitulo {
+    color: aquamarine;
+    text-align: center;
+}
+
+.imagem {
+    height: 400px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+}
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Teste</h1>
+            <h2 className="subtitulo">Teste2</h2>
+            <img className="imagem" src="https://i.imgur.com/Duy32P4.jpeg" alt="Tela Inicial" />
+        </>
+    );
+}
+
+export default Home;
