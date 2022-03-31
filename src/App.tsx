31949f09318b1f6f6768829acb00825b28a0e827
Geraldo Vedrossi Neto <73123156+geraldovedrossi@users.noreply.github.com>
2022-03-31 18:22:20 -0300

    Subindo postagens e temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,71 @@
 import React from 'react';
-
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
-
+import CadastroPostagem from './components/postagens/cadastropostagem/CadastroPostagem';
+import CadastroTema from './components/temas/cadastrotema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletartema/DeletarTema';
 
 
 function App() {
   return (
     <Router>
-
       <Navbar />
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
 
-        <Switch>
+          <Route exact path='/'>
+            <Login />
+          </Route>
 
-          <div style={{minHeight: '100vh'}}>
+          <Route path='/login'>
+            <Login />
+          </Route>
 
-            <Route exact path='/'>
-              <Login />
-            </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
 
-            <Route path='/login'>
-              <Login />
-            </Route>
+          <Route path='/cadastrousuario'>
+            <CadastroUsuario />
+          </Route>
+          <Route path='/temas'>
+            <ListaTema />
+          </Route>
+          <Route path='/posts'>
+            <ListaPostagem />
+          </Route>
 
-            <Route path='/home'>
-              <Home />
-            </Route>
+          <Route exact path='/formularioPostagem'>
+            <CadastroPostagem />
+          </Route>
+          <Route exact path='/formularioPostagem/:id'>
+            <CadastroPostagem />
+          </Route>
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+          <Route path='/deletarPostagem/:id'>
+            <DeletarPostagem />
+          </Route>
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
 
-            <Route path='/cadastrousuario'>
-              <CadastroUsuario />
-            </Route>
 
-            <Route path='/temas'>
-              <ListaTema />
-            </Route>
-
-            <Route path='/posts'>
-              <ListaPostagem />
-            </Route>
-
-          </div>
-
-        </Switch>
-
+        </div>
+      </Switch>
       <Footer />
-
     </Router>
   );
 }
--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
 import './Navbar.css';
 
 function Navbar() {
+
+    const [token, setToken] = useLocalStorage('token');
+
+    let history = useHistory();
+
+    function goLogout() {
+        setToken('');
+        alert('Usuário deslogado!');
+        history.push('/login');
+    }
+
     return (
         <>
             <AppBar position="static">
@@ -25,31 +37,33 @@ function Navbar() {
                             </Box>
                         </Link>
                         <Link to='/posts' className='text-decorator-none'>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                postagens
-                            </Typography>
-                        </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    postagens
+                                </Typography>
+                            </Box>
                         </Link>
                         <Link to='/temas' className='text-decorator-none'>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
-                            </Typography>
-                        </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    temas
+                                </Typography>
+                            </Box>
                         </Link>
+                        <Link to='/formularioTema' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 cadastrar tema
                             </Typography>
                         </Box>
-                        <Link to='/login' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
+                        </Link>
+
+                            <Box mx={1} className='cursor' onClick={goLogout}>
                                 <Typography variant="h6" color="inherit">
                                     logout
                                 </Typography>
                             </Box>
-                        </Link>
+
                     </Box>
 
                 </Toolbar>
